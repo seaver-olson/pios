05cb2ad7155d7c3ae4bf13146005f6ab1db4e4fa
Seaver Olson <sjolson007@gmail.com>
2024-08-27 16:48:00 -0500

    finished add_list()

--- src/list.c
+++ src/list.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include "list.h"
 
-void list_add(struct list_element *list_head, struct list_element new_element){
-	if (list_head->next == NULL){
-		list_head->next = &new_element;
-		return;
+void list_add(struct list_element *list_head, struct list_element *new_element){
+	if (list_head == NULL){
+		list_head = new_element;
 	}
 	while (list_head->next != NULL){
-		//printf("%d",list_head->data);
 		list_head = list_head->next;
 	}
-	list_head->next = &new_element;
+	list_head->next = new_element;
 }
 void list_remove(struct list_element *element);
 
 void list_items(struct list_element *head){
-	while (head->next != NULL){
+	while (head != NULL){
 		printf("%d",head->data);
 		head = head->next;
 	}
@@ -27,13 +25,13 @@ int main(){
 	struct list_element a = {&b , 4};
 	struct list_element d = {NULL, 7};
 	struct list_element e = {NULL, 9};
+	struct list_element *new_element = &d;
 	struct list_element *head = &a;
-	list_add(head,d);
-	list_items(head);
-	list_add(head,e);
-	list_items(head);
-	for (int i=0;i < 4;i++){
-		printf("%d",head->data);
+	list_add(head,new_element);
+	new_element = &e;
+	list_add(head,new_element);
+	while(head != NULL){
+		printf("%d", head->data);
 		head = head->next;
 	}
 	return 0;
