96142500296d8630483b60e7896f95e7eb59bef9
seaver-olson <sjolson007@gmail.com>
2024-08-27 13:56:07 -0500

    list.c still in development

--- src/list.c
+++ src/list.c
@@ -2,18 +2,26 @@
 #include "list.h"
 
 void list_add(struct list_element *list_head, struct list_element 
-*new_element,int index);
-
-void print(int index){
-	if (index){
-		printf("%s", "lol");
-	} else{
-		printf("%s", "hello");
-}}
+*new_element){
+	while (list_head.next != NULL){
+		list_head = list_head.next;
+	}
+	list_head.next = new_element;
+}
 void list_remove(struct list_element *element);
 
 int main(){
-	print();
+	struct c = {NULL, 5};
+	struct b = {&c, 4};
+	struct a = {&b , 4};
+	struct new = {NULL, 7};
+	list_add(head,new);
+	printf("%s","testing:");
+	struct list_element *head = &a;
+	while(true){
+		printf("%d",head.data);
+		head.next;
+	}
 	return 0;
 }
 	
