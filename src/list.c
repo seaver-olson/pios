bfaffd4ef2f709eb6d3eff1d51646b863f746a25
Seaver Olson <sjolson007@gmail.com>
2024-08-27 15:02:02 -0500

    worked on add_list() stopped at segmentation fault because I have work

--- src/list.c
+++ src/list.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include "list.h"
 
-void list_add(struct list_element *list_head, struct list_element 
-*new_element){
-	while (list_head.next != NULL){
-		list_head = list_head.next;
+void list_add(struct list_element *list_head, struct list_element new_element){
+	while (list_head->next != NULL){
+		list_head = list_head->next;
 	}
-	list_head.next = new_element;
+	list_head->next = &new_element;
 }
 void list_remove(struct list_element *element);
 
 int main(){
-	struct c = {NULL, 5};
-	struct b = {&c, 4};
-	struct a = {&b , 4};
-	struct new = {NULL, 7};
-	list_add(head,new);
-	printf("%s","testing:");
+	struct list_element c = {NULL, 5};
+	struct list_element b = {&c, 4};
+	struct list_element a = {&b , 4};
+	struct list_element d = {NULL, 7};
 	struct list_element *head = &a;
-	while(true){
-		printf("%d",head.data);
-		head.next;
+	list_add(head,d);
+	printf("%s","testing:");
+	while(head != NULL){
+		printf("%d",head->data);
+		head = head->next;
 	}
 	return 0;
 }
