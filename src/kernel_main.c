86a2acbc67baa7914e6192b7be5cc15e4f4702d4
Seaver Olson <sjolson007@gmail.com>
2024-08-27 17:58:04 -0500

    started kernel_main.c however ran into memory error

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,8 +1,14 @@
+extern unsigned char __bss_start;
+extern unsigned char __bss_end;
 
-
+#include <stdio.h>
 
 void kernel_main() {
-
-    while(1){
+	char *begin_bss = &__bss_start;
+	char *end_bss = &__bss_end;
+	//testing where the bss begins and ends, vars aren't working
+	printf("%d", &begin_bss);
+	printf("%d", &end_bss);
+	while(1){
     }
 }
