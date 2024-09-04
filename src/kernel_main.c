cd0c16ae3723d3d5bf1a315f578e3a7feafefb75
seaver-olson <sjolson007@gmail.com>
2024-09-04 16:53:31 -0500

    finished clearing the bss buffer

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -2,12 +2,17 @@
 extern unsigned char __bss_start;
 extern unsigned char __bss_end;
 
+
 void kernel_main() {
 	char *begin_bss = &__bss_start;
 	char *end_bss = &__bss_end;
-	//testing where the bss begins and ends, vars aren't working
-	printf("%d", &begin_bss);
-	printf("%d", &end_bss);
+	char *currentCell = &begin_bss;
+	//clear bss buffer	
+	while(&currentCell != &end_bss){
+		currentCell = '0';
+		*currentCell = &currentCell++;		
+	} 
 	while(1){
-    }
+    	
+	}
 }
