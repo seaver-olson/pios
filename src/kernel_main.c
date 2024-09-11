195ac0cce1382714d96c5cfdaed0c632975bf31e
seaver-olson <sjolson007@gmail.com>
2024-09-11 17:09:51 -0500

    added to kernel_main

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -2,7 +2,10 @@
 extern unsigned char __bss_start;
 extern unsigned char __bss_end;
 
-
+unsigned long get_timer_count(){
+	unsigned long *timer_count_register = 0x3f003004;
+	return *timer_count_register;
+}
 void kernel_main() {
 	char *begin_bss = &__bss_start;
 	char *end_bss = &__bss_end;
