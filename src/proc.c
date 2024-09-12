b75ff1de41771b84d3cd4d25044eb8c9bea0ae3b
seaver-olson <sjolson007@gmail.com>
2024-09-12 10:56:46 -0500

    finished proc.c

---
+++ src/proc.c
@@ -0,0 +1,29 @@
+struct context{
+	int eip;
+	int esp;
+	int ebx;
+	int ecx;
+	int edx;
+	int esi;
+	int edi;
+	int ebp;
+};
+
+//going to be used for virtualization coordination
+enum proc_state { UNUSED,EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
+
+struct proc {
+	char *mem;//start of memory address
+	uint sz;//size
+	char *kstack;//bottom of kernel stack
+
+	enum proc_state state;
+	int pid;
+	struct proc *parent;
+	void *chan; //if !zero, sleeping on chan
+	int killed; //if !zero, has been killed
+	struct file *ofile[NOFILE]; // opens files
+	struct inode *cwd; //current working directory
+	struct context context; //switch here to run process
+	struct trapfram *tf;//trap frame for the current interrupt
+};
