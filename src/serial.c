d81e77c41c93bb1b8cf512bbd15b11daaa6807fb
seaver-olson <sjolson007@gmail.com>
2024-09-26 05:44:06 +0100

    currently working on the serial.c

--- src/serial.c
+++ src/serial.c
@@ -1,9 +1,14 @@
 #include "rprintf.h"
 
-#define MU_IO_REG     (*(volatile int *)0x3F215040)
+#define MU_IO_REG 0x3F00000
+
+volatile int *muio_ptr = (volatile int *)(MU_IO_REG);
 
 void putc(int data){
-	MU_IO_REG = data;
+	if (muio_ptr != NULL){
+		*muio_ptr = data;
+	}
+	//reverse would be data = *muio_ptr; to read the value at muio
 }
 
 int getEL(){
