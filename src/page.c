e2f0b85eab1f49410b23a6667baf143f5d463d1e
seaver-olson <sjolson007@gmail.com>
2024-10-01 02:01:24 +0100

    finished init_pfa_list(void)

--- src/page.c
+++ src/page.c
@@ -3,13 +3,16 @@
 struct ppage physical_page_array[128];
 
 void init_pfa_list(void) {
-	struct ppage *free_physical_pages;
-	struct ppage *current_physical_page = &physical_page_array[0];
+	struct ppage *free_physical_pages = &physical_page_array[0];
+	struct ppage *current_physical_page;
 	for (int i = 0; i < 128;i++){
+		current_physical_page = &physical_page_array[i+1]; //swapped the initizaitions  of curr_p_p and free_p_p, fixed seg fault
 		free_physical_pages->next = current_physical_page;
 		current_physical_page->prev = free_physical_pages;
+
+		free_physical_pages  = current_physical_page;
 	}
-}	
+}
 
 struct ppage *allocate_physical_pages(unsigned int npages){
 
