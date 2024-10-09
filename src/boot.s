604efae4189d4d990b3b96f0554ecc4fcbf75dcc
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2024-10-09 16:44:54 -0500

    Update boot.s

--- src/boot.s
+++ src/boot.s
@@ -28,61 +28,61 @@ b _start           /* CODE0 : Executable code */
 .balign 0x800
 _vectors:
 curr_el_sp0_sync:        // The exception handler for a synchronous 
-                         // exception from the current EL using SP0.
+        b .                  // exception from the current EL using SP0.
 .balign 0x80
 curr_el_sp0_irq:         // The exception handler for an IRQ exception
-                         // from the current EL using SP0.
+        b .                 // from the current EL using SP0.
 .balign 0x80
 curr_el_sp0_fiq:         // The exception handler for an FIQ exception
-                         // from the current EL using SP0.
+        b .                 // from the current EL using SP0.
 .balign 0x80
 curr_el_sp0_serror:      // The exception handler for a System Error 
-                         // exception from the current EL using SP0.
+        b .                // exception from the current EL using SP0.
 .balign 0x80
 curr_el_spx_sync:        // The exception handler for a synchrous 
-                         // exception from the current EL using the
+        b .                 // exception from the current EL using the
                          // current SP.
 .balign 0x80
 curr_el_spx_irq:         // The exception handler for an IRQ exception from 
-                         // the current EL using the current SP.
+        b .                 // the current EL using the current SP.
 
 .balign 0x80
 curr_el_spx_fiq:         // The exception handler for an FIQ from 
-                         // the current EL using the current SP.
+        b .                // the current EL using the current SP.
 
 .balign 0x80
 curr_el_spx_serror:      // The exception handler for a System Error 
-                         // exception from the current EL using the
+        b .                 // exception from the current EL using the
                          // current SP.
 
  .balign 0x80
 lower_el_aarch64_sync:   // The exception handler for a synchronous 
-                         // exception from a lower EL (AArch64).
+         b .                // exception from a lower EL (AArch64).
 
 .balign 0x80
 lower_el_aarch64_irq:    // The exception handler for an IRQ from a lower EL
-                         // (AArch64).
+         b .                // (AArch64).
 
 .balign 0x80
 lower_el_aarch64_fiq:    // The exception handler for an FIQ from a lower EL
-                         // (AArch64).
+         b .                // (AArch64).
 
 .balign 0x80
 lower_el_aarch64_serror: // The exception handler for a System Error 
-                         // exception from a lower EL(AArch64).
+         b .                // exception from a lower EL(AArch64).
 
 .balign 0x80
 lower_el_aarch32_sync:   // The exception handler for a synchronous 
-                         // exception from a lower EL(AArch32).
+         b .                // exception from a lower EL(AArch32).
 .balign 0x80
 lower_el_aarch32_irq:    // The exception handler for an IRQ exception 
-                         // from a lower EL (AArch32).
+         b .                // from a lower EL (AArch32).
 .balign 0x80
 lower_el_aarch32_fiq:    // The exception handler for an FIQ exception from 
-                         // a lower EL (AArch32).
+         b .                // a lower EL (AArch32).
 .balign 0x80
 lower_el_aarch32_serror: // The exception handler for a System Error
-                         // exception from a lower EL(AArch32).
+         b .               // exception from a lower EL(AArch32).
 
 
 
